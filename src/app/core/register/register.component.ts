c4a5d1cb3068d537feb4347261be997e688bf7d0
Taharder <50746611+sahraouifaris@users.noreply.github.com>
2023-06-12 17:54:51 +0200

    register

--- src/app/core/register/register.component.ts
+++ src/app/core/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit,ViewChild,ElementRef } from '@angular/core';
 import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { HttpClient} from "@angular/common/http";
 import { Router } from '@angular/router';
+import { Position } from 'src/app/Model/Position'
+
 
 @Component({
   selector: 'app-register',
@@ -16,7 +18,8 @@ selectedRole: string = "student";
 url ="http://localhost:5000/api/";
 @ViewChild("address") addressDiv!:ElementRef;
 
-
+public Latitude:string = "0";
+public Longitude: string = "0";
   constructor( 
     private fb:UntypedFormBuilder,
     private http:HttpClient,
@@ -29,10 +32,12 @@ this.form = this.fb.group( {
   name:["",Validators.required],
   password:"",
   email:"",
-
-      price:"",
-      field:"",
-      experience:""
+  Phone:"",
+  pricePerHour:"",
+  field:"",
+  experienceYears:"",
+  Longitude:"",
+  Latitude:"",
     }
     )
 }
@@ -44,13 +49,14 @@ submit(){
   if (this.selectedRole == "student") {
     this.http.post(this.url+"students",this.form.getRawValue())
     .subscribe(res=>{
+      console.log(res)
       this.router.navigateByUrl("/login")
     })
   }
    else {
     console.log(this.form.getRawValue())
     
-    this.http.post(this.url+"speaker/register",this.form.getRawValue())
+    this.http.post(this.url+"teachers",this.form.getRawValue())
     .subscribe(res=>{
       
       
@@ -72,11 +78,32 @@ if (this.selectedRole!="student")
   this.addressDiv.nativeElement.style = "display:block" 
 }
 }
-
-
-    
-  
   ngOnInit(): void {
+  }
+
+
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position: Position) => {
+        if (position) {
+          console.log("Latitude: " + position.coords.latitude +
+            " Longitude: " + position.coords.longitude);
+          this.Latitude = position.coords.latitude.toString();
+          this.Longitude = position.coords.longitude.toString();
+          console.log(this.Latitude);
+          console.log(this.Longitude);
+          this.form.patchValue({
+            Latitude: this.Latitude,
+            Longitude: this.Longitude
+          });
+        }
+      },
+        (error) => {
+          return console.log(error);
+        });
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
   }
 
 }
