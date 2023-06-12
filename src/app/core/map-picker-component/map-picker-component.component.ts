66edcfd6910e344a1f45a3b60d142c57fb08cbd8
Mohamed-2000 <mogenaidy@gmail.com>
2023-06-12 18:42:11 +0300

    F

--- src/app/core/map-picker-component/map-picker-component.component.ts
+++ src/app/core/map-picker-component/map-picker-component.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AuthService } from 'src/app/auth.service';
 
 @Component({
   selector: 'app-map-picker-component',
@@ -7,9 +8,129 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapPickerComponentComponent implements OnInit {
 
-  constructor() { }
+  highestRatedTeachers: Teacher[] = [];
+  teachers: Teacher[] = [];
+  private userLatitude: number= 0 ;
+  private userLongitude: number = 0 ;
+  role:any;
+  map: any;
+  name:any;
+  Id:any;
+  islogin: any;
+  authentic:boolean=false;
 
-  ngOnInit(): void {
+
+
+  @Input('rating') private rating: number = 3;
+  @Input('starCount') private starCount: number = 5;
+  @Input('color') public color: string = 'accent';
+  @Output() private ratingUpdated = new EventEmitter();
+
+  private snackBarDuration: number = 2000;
+  public ratingArr = [];
+
+  constructor(public auth:AuthService, private teacherService:TeacherService,
+      public login:AuthService) {
+
+      if(localStorage.getItem("jwt_token")){
+        this.login.isloggedin = true;
+      }
+      else{
+        this.login.isloggedin = false;
+      }
+      this.name = localStorage.getItem("name");
+      this.role=localStorage.getItem("Role");
+      this.Id = localStorage.getItem("Id");
+
+     }
+
+  ngOnInit() {
+    this.getUserLocation();
+    this.role =localStorage.getItem('Role');
+    this.getActivatedTeachers();
+     this.getTeachersWithHighstRate();
+     this.name = localStorage.getItem("name");
+     this.login.getname().subscribe(res=>{
+       this.name =res;
+     })
+       this.login.getIsLogin().subscribe(res=>{
+       this.islogin = res;
+
+
+     });
+
   }
 
+  getUserLocation() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.userLatitude = position.coords.latitude;
+        this.userLongitude = position.coords.longitude;
+        this.initializeMap();
+      },
+      (error) => {
+        console.error('Error getting user location:', error);
+      }
+    );
+  }
+
+  initializeMap() {
+    L.Icon.Default.imagePath = "./assets/images/";
+
+    this.map = L.map('map').setView([this.userLatitude, this.userLongitude], 10);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
+      maxZoom: 18
+    }).addTo(this.map);
+
+    // Place a marker at the user's location
+    L.marker([this.userLatitude, this.userLongitude])
+      .addTo(this.map)
+      .bindPopup('Your Location');
+
+    // Place markers for nearby teachers
+    this.teachers.forEach((teacher) => {
+      console.log(teacher);
+      if(teacher.Latitude != undefined){
+      L.marker([teacher.Latitude, teacher.Longitude])
+        .addTo(this.map)
+        .bindPopup(`<strong>${teacher.name}</strong><br>Rating: ${teacher.rating}`);
+      }
+    });
+
+  }
+
+
+  getTeachersWithHighstRate() {
+    this.teacherService.getTeachersWithHighestRate()
+      .subscribe(response => {
+        if (response) {
+          this.highestRatedTeachers = response.data.slice(0, 8);
+        }
+      }, error => {
+        console.log(error);
+      });
+  }
+
+
+  getActivatedTeachers() {
+    this.teacherService.GetActiveTeachers()
+      .subscribe(response => {
+      if (response) {
+        this.teachers = response.data;
+
+      }
+    }, error => {
+      console.log(error);
+
+    });
+  }
+
+
+}
+export enum StarRatingColor {
+  primary = "primary",
+  accent = "accent",
+  warn = "warn"
 }
